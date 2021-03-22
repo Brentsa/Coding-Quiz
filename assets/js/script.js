bf3241e583374d48360d87c92c7cfa290354b232
Solidshots <seanbrent5@gmail.com>
2021-03-21 22:25:59 -0400

    Final Version 1.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,60 +21,69 @@ var questionCount = 0;
 var totalQuestions = 5;
 var pageIndex = 1;
 
-
-//Return a random number between a supplied range
-function randBetween(min, max){
-    return Math.floor(Math.random() * (max + 1 - min)) + min;
-}
-
+//Begin the time countdown and decrement the users score every second
 function startCountdown(){
     timer = setInterval(function(){
-        if(time > 0){
-            time--;
+        //only decrement the score if the user has score left
+        time--;
+        document.querySelector("#time").textContent = "Score: " + time;
+
+        if(time < 0){
+            document.querySelector(".question-block").remove();
+            alert("Game Over!");
+            initQuiz();
         }
-        document.querySelector("#time").textContent = "Time: " + time;
     },1000);
 }
 
+//End the time countdown
 function endCountdown(){
     clearInterval(timer);
 }
 
+//Determines the html to render based on the button clicked
 function handleButtonPress(event){
 
+    //checks if button was pressed
     if(event.target.matches("button")){
-
+        
+        //If a button with the clear class is pressed, clear the highscores
         if(event.target.matches(".clear-hs")){
             highScores = [];
             saveScores();
         }
 
         if(pageIndex === -1){
+            //only called when we check the highscore via the top left button
             document.querySelector(".question-block").remove();
             initQuiz();
         }
         else if(pageIndex === totalQuestions + 3){
+            //restarts the quiz if we reach the final high score page
             document.querySelector(".question-block").remove();
             initQuiz();
         }
         else{
+            //render the next page and increase the page index
             renderPage(event);
             pageIndex++;
         }
     }
 }
 
+//Renders dynamically generated HTML depending on the page index we are on
 function renderPage(event){
 
     switch(pageIndex){
+        //renders the first question and starts the quiz and countdown
         case (1):
             document.querySelector(".question-block").remove();
             initQuizQuestion();
             startCountdown();
             break;
 
+        //renders the submit score page when all of the questions have been answered
         case (totalQuestions + 1):
-
             var questionCorrect = isQuestionCorrect(event);
 
             document.querySelector(".question-block").remove();
@@ -84,6 +93,7 @@ function renderPage(event){
             printAnswer(questionCorrect);
             break;
 
+        //renders the high scores after a score has bee submitted
         case (totalQuestions + 2):
             createScoreObject();
             document.querySelector(".question-block").remove();
@@ -91,8 +101,8 @@ function renderPage(event){
             initHighScoresList();
             break;
 
+        //renders the question based on the page index, it will always render through all the questions
         default:
-        
             var questionCorrect = isQuestionCorrect(event);
 
             document.querySelector(".question-block").remove();
@@ -103,19 +113,20 @@ function renderPage(event){
     }
 }
 
+//Returns true or false if the user selected the right or wrong answer
 function isQuestionCorrect(event){
     if(event.target.textContent === questions[questionCount-1].corAnswer){
-        console.log("correct");
         return true;
     }
     else{
-        console.log("wrong");
-        time -= 20;
-        document.querySelector("#time").textContent = "Time: " + time;
+        //subtract 20 points from the user time/score and do not allow the score to go below 0
+        time = Math.max(0, time - 20);
+        document.querySelector("#time").textContent = "Score: " + time;
         return false;
     }
 }
 
+//Print if the answer given by the user is correct or not in the log
 function printAnswer(isCorrect){
     var questionStatus = document.querySelector(".status-box");
     if(isCorrect){
@@ -126,6 +137,7 @@ function printAnswer(isCorrect){
     }     
 }
 
+//We can only view the high scores from the quiz start page and then sets the index to -1 to restart the quiz
 function viewScores(event){
     if(pageIndex == 1){
         document.querySelector(".question-block").remove();
@@ -134,25 +146,29 @@ function viewScores(event){
     }
 }
 
+//Called whenever the user starts a new quiz scenario
 function initQuiz(){
+    //Resets game parameters and loads scores from storage
+    endCountdown();
     time = 100;
     questionCount = 0;
     pageIndex = 1;
-    document.querySelector("#time").textContent = "Time: " + time;
-
+    document.querySelector("#time").textContent = "Score: " + time;
     loadScores();
 
     initStartScreen();
 }
 
+//Builds the start screen page html
 function initStartScreen(){
     var initialBlock = document.createElement("div");
     initialBlock.className = "initial-block question-block";
-    initialBlock.innerHTML = "<h1 class='block-item'>Coding Quiz</h1><p class='block-item'>This is a quiz meant to text your knowledge of Javascript. Press start to begin!</p><button id='start-btn' class='btn centered block-item' type='click'>Start</button>";
+    initialBlock.innerHTML = "<h1 class='block-item'>Coding Quiz</h1><p class='block-item'>This is a coding quiz meant to text your knowledge of Javascript. Once the game has started, you will lose one score per second. If you answer a question incorrectly, you will lose 20pts. Press the start button below to begin!</p><button id='start-btn' class='btn centered block-item' type='click'>Start</button>";
 
     quizBlockMain.appendChild(initialBlock);
 }
 
+//Builds quiz question html with possible answers
 function initQuizQuestion(){
     var questionBlock = document.createElement("div");
     questionBlock.className = "question-block";
@@ -171,6 +187,7 @@ function initQuizQuestion(){
     questionCount++;
 }
 
+//Builds the answers that are selectable by the user in button format
 function createQuizButtons(answersArray){
 
     var buttonContainer = document.createElement("div");
@@ -186,6 +203,7 @@ function createQuizButtons(answersArray){
     return buttonContainer;
 }
 
+//Builds the html that allows the user to enter a high score
 function initHighScore(){
     var textBlock = document.createElement("div");
     textBlock.className = "question-block";
@@ -217,12 +235,14 @@ function initHighScore(){
     quizBlockMain.appendChild(textBlock);
 }
 
+//Adds the score object entered by the user into our highscore array
 function createScoreObject(){
     var initials = document.querySelector("input[name='hs-input'").value;
     var scoreItem = {initials: initials, score: time};
     highScores.push(scoreItem);
 }
 
+//Generic function that allows us to sort objects in an array by a specified key
 function sort_by_key(array, key){
     return array.sort(function(a, b){
         var x = a[key]; var y = b[key];
@@ -230,6 +250,7 @@ function sort_by_key(array, key){
     });
 }
 
+//Sorts the high scores from highest to lowest and then builds the html to display the scores
 function initHighScoresList(){
 
     sort_by_key(highScores, 'score');
@@ -256,6 +277,7 @@ function initHighScoresList(){
         hsBlock.appendChild(highScoreBlock);
     }
 
+    //Changes the buttons available to the user if they click to view high scores or arrive there by reaching the end of the quiz loop
     if(questionCount === totalQuestions){
         var button1 = document.createElement("button");
         button1.className = "btn centered block-item play-again";
@@ -279,10 +301,12 @@ function initHighScoresList(){
     quizBlockMain.appendChild(hsBlock);
 }
 
+//Saves the scores in our score array to local storage
 function saveScores(){
     localStorage.setItem("scores", JSON.stringify(highScores));
 }
 
+//Loads the scores from our local storage to our global array
 function loadScores(){
     var savedScores = localStorage.getItem("scores");
 
@@ -291,8 +315,11 @@ function loadScores(){
     highScores = JSON.parse(savedScores);
 }
 
+//Handles all button press clicks
 quizBlockMain.addEventListener("click", handleButtonPress);
 
+//Handles the click when a user wants to check the high scores in the start screen
 viewHighScores.addEventListener("click", viewScores);
 
+//Start the quiz when the page loads
 initQuiz();
